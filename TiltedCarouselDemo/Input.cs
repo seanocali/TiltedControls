8dba2202fed2e9f8736c67213ad35c9a7087fec0
seanocali <seanoneilcalifornia@gmail.com>
2020-12-13 21:03:45 -0800

    Test push.

--- TiltedCarouselDemo/Input.cs
+++ TiltedCarouselDemo/Input.cs
@@ -19,6 +19,7 @@ namespace TiltedCarouselDemo
         {
             if (carousel != null && carousel.AreItemsLoaded)
             {
+                //GitTest
                 carousel.StartManipulationMode();
             }
         }
