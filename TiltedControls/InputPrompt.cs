4b2676de27ab53f23e2022c6c146095448ece206
Sean O'Neil <seanoneilcalifornia@gmail.com>
2021-03-11 02:47:51 -0800

    Monochrome keyboard now uses svg images instead of font.

--- TiltedControls/InputPrompt.cs
+++ TiltedControls/InputPrompt.cs
@@ -10,6 +10,7 @@ using Windows.Storage;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using static TiltedControls.Common;
@@ -24,7 +25,6 @@ namespace TiltedControls
         Assembly _assembly;
         CancellationTokenSource _cts;
 
-        static FontFamily _keyboardMonochromeFont;
         static FontFamily _xboxOneMonochromeFont;
         static FontFamily _ps4MonochromeFont;
 
@@ -95,21 +95,15 @@ namespace TiltedControls
             string monochromeFontName = Monochrome != MonochromeModes.None ? GetMonochromeFontName(rootFolderName, productName, Monochrome == MonochromeModes.Force) : null;
             if (monochromeFontName != null)
             {
+
                 var vb = new Viewbox();
                 var tb = new TextBlock();
                 tb.Foreground = this.Foreground;
                 tb.FontFamily = LoadAndGetFont(monochromeFontName);
-                if (monochromeFontName == "keyboard")
-                {
-                    tb.Text = MappedKeyboardKey != null ? MappedKeyboardKey : GetDefaultKeyboardKeyName(key);
-                }
-                else
-                {
-                    var hexStr = "E8" + ((int)key).ToString("X2");
-                    int decValue = int.Parse(hexStr, System.Globalization.NumberStyles.HexNumber);
-                    var c = (char)decValue;
-                    tb.Text = c.ToString();
-                }
+                var hexStr = "E8" + ((int)key).ToString("X2");
+                int decValue = int.Parse(hexStr, System.Globalization.NumberStyles.HexNumber);
+                var c = (char)decValue;
+                tb.Text = c.ToString();
                 vb.Child = tb;
                 this.Content = vb;
             }
@@ -131,7 +125,7 @@ namespace TiltedControls
 
                 string resourceName = $"TiltedControls.Resources.InputPromptImages.{rootFolderName}.{themeName}{productName}{keyName}.svg";
 
-                await UpdateImage(resourceName, cancellationToken);
+                await UpdateImageFromEmbeddedResource(resourceName, cancellationToken);
             }
         }
 
@@ -139,9 +133,6 @@ namespace TiltedControls
         {
             switch (requestedFont)
             {
-                case "keyboard":
-                    if (_keyboardMonochromeFont == null) { _keyboardMonochromeFont = new FontFamily("/TiltedControls/Resources/Fonts/keyboard.ttf#keyboard"); }
-                    return _keyboardMonochromeFont;
                 case "xboxone":
                     if (_xboxOneMonochromeFont == null) { _xboxOneMonochromeFont = new FontFamily("/TiltedControls/Resources/Fonts/xboxone.ttf#xboxone"); }
                     return _xboxOneMonochromeFont;
@@ -152,7 +143,7 @@ namespace TiltedControls
             return null;
         }
 
-        private async Task UpdateImage(string resourceName, CancellationToken cancellationToken)
+        private async Task UpdateImageFromEmbeddedResource(string resourceName, CancellationToken cancellationToken)
         {
             bool success = false;
             try
@@ -191,6 +182,39 @@ namespace TiltedControls
             else { OnImageLoadFailed(); }
         }
 
+        private async Task UpdateImageFromSvgString(string svgString, CancellationToken cancellationToken)
+        {
+            bool success = false;
+            try
+            {
+                using (var memStream = new MemoryStream())
+                using (var writer = new StreamWriter(memStream))
+                {
+                    memStream.Position = 0;
+                    writer.Write(memStream);
+                    writer.Flush();
+                    memStream.Position = 0;
+                    using (var raStream = memStream.AsRandomAccessStream())
+                    {
+                        var setSourceTask = _source.SetSourceAsync(raStream).AsTask();
+                        await Task.WhenAny(setSourceTask, cancellationToken.AsTask());
+                        if (!cancellationToken.IsCancellationRequested)
+                        {
+                            SvgImageSourceLoadStatus status = await setSourceTask;
+                            success = status == 0;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (success) { OnImageLoaded(); }
+            else { OnImageLoadFailed(); }
+        }
+
         private string GetMonochromeFontName(string vendorName, string productName, bool force = false)
         {
             if (vendorName != null)
@@ -212,10 +236,7 @@ namespace TiltedControls
                     return GetExactMonochromeGamepadName(productName);
                 }
             }
-            else
-            {
-                return "keyboard";
-            }
+            return null;
         }
 
         string GetExactMonochromeGamepadName(string productName)
@@ -672,6 +693,14 @@ namespace TiltedControls
             new PropertyMetadata(null, OnMapPropertyChanged));
 
         #endregion
+
+        static string Test
+        {
+            get
+            {
+                return @"<?xml version=""1.0"" encoding=""UTF-8""?><!--This file is compatible with Silverlight--><Canvas xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"" Name=""Layer_1"" Canvas.Left=""0"" Canvas.Top=""0"" Width=""100"" Height=""100"">  <Canvas.RenderTransform>    <TranslateTransform X=""0"" Y=""0""/>  </Canvas.RenderTransform>  <Canvas.Resources/>  <!--Unknown tag: metadata-->  <!--Unknown tag: sodipodi:namedview-->  <Canvas Name=""g69"">    <Canvas.RenderTransform>      <MatrixTransform Matrix=""1 0 0 1 0 0""/>    </Canvas.RenderTransform>    <Canvas Name=""g64"">      <Canvas Name=""Layer1_0_FILL"">        <Path xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" Name=""path61"" Fill=""#FF000000"" Data=""M52.6 55c-0.4-0.4-0.8-0.6-1.3-0.8s-1-0.3-1.6-0.3c-0.3 0-0.5 0-0.8 0.2c-0.3 0.1-0.5 0.1-0.7 0.3     c-0.2 0.1-0.4 0.2-0.5 0.4c-0.1 0.1-0.3 0.3-0.4 0.4v-1H45v11.6h2.5v-4.6h0c0.2 0.4 0.6 0.7 1 0.8c0.4 0.2 0.9 0.3 1.5 0.3     c0.5 0 1-0.1 1.5-0.4c0.4-0.2 0.8-0.5 1.2-1c0.3-0.4 0.5-0.8 0.7-1.3c0.1-0.5 0.2-1 0.2-1.5c0-0.6-0.1-1.2-0.3-1.7     C53.1 55.8 52.9 55.4 52.6 55 M50.7 56.5c0.3 0.4 0.5 0.8 0.5 1.5c0 0.6-0.2 1.1-0.5 1.5c-0.3 0.4-0.8 0.5-1.5 0.5     s-1.1-0.2-1.5-0.5c-0.3-0.4-0.5-0.8-0.5-1.5s0.2-1.1 0.5-1.5c0.3-0.4 0.8-0.5 1.5-0.5S50.4 56.2 50.7 56.5 M62.3 58     c0-0.7-0.1-1.3-0.2-1.8c-0.1-0.5-0.3-0.9-0.6-1.3c-0.3-0.3-0.6-0.6-1-0.8c-0.5-0.2-1.1-0.3-1.8-0.3c-0.6 0-1.3 0.1-1.9 0.3     c-0.6 0.2-1.1 0.5-1.5 0.9l1.3 1.4c0.2-0.3 0.5-0.5 0.8-0.7c0.4-0.2 0.8-0.3 1.2-0.3s0.8 0.1 1 0.4c0.3 0.2 0.5 0.6 0.5 1     c-0.3 0-0.7 0-1.1 0c-0.4 0-0.8 0-1.2 0c-0.4 0.1-0.8 0.2-1.2 0.3c-0.4 0.1-0.7 0.3-1 0.5c-0.3 0.2-0.5 0.5-0.7 0.8     c-0.1 0.3-0.2 0.7-0.2 1.2c0 0.4 0.1 0.8 0.2 1c0.2 0.3 0.4 0.6 0.6 0.8c0.3 0.2 0.6 0.4 1 0.5c0.3 0.1 0.7 0.2 1 0.2     c0.5 0 0.9-0.1 1.4-0.3c0.4-0.2 0.8-0.5 1.1-1v1h2.3V58 M58.7 58.5c0.3 0 0.5 0 0.7 0H60V59c0 0.3 0 0.5-0.1 0.7     c-0.1 0.2-0.3 0.3-0.5 0.4c-0.2 0.1-0.4 0.2-0.6 0.3c-0.2 0.1-0.4 0.1-0.7 0.1c-0.2 0-0.5-0.1-0.8-0.3c-0.3-0.1-0.4-0.4-0.4-0.7     c0-0.2 0.1-0.4 0.3-0.6c0.2-0.2 0.4-0.3 0.7-0.3C58.2 58.6 58.5 58.5 58.7 58.5 M69.4 54.1c-0.5-0.2-1-0.3-1.5-0.3     c-0.6 0-1.1 0.1-1.7 0.3c-0.5 0.2-1 0.4-1.4 0.8c-0.4 0.4-0.7 0.8-0.9 1.3c-0.2 0.5-0.4 1.1-0.4 1.7c0 0.6 0.1 1.2 0.4 1.7     c0.2 0.5 0.6 1 0.9 1.3c0.4 0.3 0.9 0.6 1.4 0.8s1.1 0.3 1.7 0.3c0.5 0 1-0.1 1.5-0.3c0.5-0.1 0.9-0.4 1.3-0.8l-1.6-1.7     c-0.1 0.2-0.3 0.3-0.5 0.4C68.5 60 68.2 60 67.9 60c-0.6 0-1.1-0.2-1.4-0.5c-0.4-0.4-0.6-0.8-0.6-1.5s0.2-1.1 0.6-1.5     c0.3-0.4 0.8-0.5 1.4-0.5c0.3 0 0.5 0 0.8 0.2c0.2 0.1 0.4 0.2 0.5 0.4l1.6-1.7C70.4 54.5 69.9 54.2 69.4 54.1 M78.5 55     c-0.3-0.4-0.7-0.6-1.2-0.8c-0.5-0.2-1-0.3-1.5-0.3c-0.6 0-1.2 0.1-1.7 0.3c-0.5 0.2-1 0.4-1.4 0.8c-0.4 0.4-0.7 0.8-0.9 1.3     c-0.2 0.5-0.3 1.1-0.3 1.7c0 0.6 0.1 1.2 0.3 1.7s0.5 1 0.9 1.3c0.4 0.3 0.9 0.6 1.4 0.8c0.5 0.2 1.1 0.3 1.7 0.3     c0.6 0 1.3-0.1 1.9-0.4c0.6-0.3 1.1-0.7 1.5-1.2l-1.8-1.3c-0.2 0.3-0.4 0.5-0.7 0.7c-0.3 0.2-0.7 0.3-1 0.3     c-0.5 0-0.9-0.1-1.2-0.4c-0.3-0.3-0.5-0.7-0.7-1.2h5.7V58c0-0.6-0.1-1.2-0.3-1.7C79.1 55.8 78.8 55.3 78.5 55 M74.3 56.1     c0.2-0.1 0.3-0.3 0.5-0.4c0.2-0.1 0.5-0.1 0.7-0.1c0.4 0 0.8 0.2 1.1 0.5c0.3 0.3 0.4 0.6 0.4 1h-3.3c0-0.2 0.1-0.4 0.2-0.5     C74.1 56.4 74.2 56.2 74.3 56.1 M39.8 50.2c-0.6 0-1.2 0.1-1.7 0.3c-0.5 0.1-1 0.4-1.4 0.7c-0.4 0.3-0.7 0.7-1 1.2     c-0.2 0.5-0.3 1-0.3 1.6c0 0.7 0.1 1.2 0.4 1.6c0.3 0.4 0.6 0.7 1 0.9c0.4 0.3 0.9 0.5 1.3 0.6c0.4 0.2 0.9 0.3 1.3 0.4     c0.4 0.1 0.7 0.3 1 0.6c0.2 0.2 0.4 0.4 0.4 0.8c0 0.2 0 0.4-0.1 0.5c-0.1 0.1-0.2 0.3-0.4 0.3c-0.2 0.1-0.4 0.2-0.6 0.3     s-0.4 0.1-0.6 0.1c-0.4 0-0.8-0.1-1.2-0.3c-0.4-0.2-0.7-0.4-1-0.8l-1.8 2c0.6 0.5 1.2 0.9 1.8 1.1c0.7 0.2 1.4 0.3 2 0.3     c0.6 0 1.2-0.1 1.8-0.2c0.5-0.1 1-0.4 1.4-0.7c0.4-0.3 0.7-0.7 1-1.2c0.2-0.5 0.3-1 0.3-1.6c0-0.7-0.1-1.3-0.3-1.7     c-0.3-0.4-0.6-0.7-1-0.9c-0.4-0.3-0.8-0.5-1.3-0.6c-0.4-0.2-0.8-0.3-1.3-0.4c-0.4-0.1-0.7-0.3-1-0.5C38.1 54.3 38 54 38 53.7     c0-0.2 0-0.4 0.1-0.6c0.1-0.1 0.3-0.2 0.5-0.3c0.2-0.1 0.3-0.2 0.5-0.2c0.2-0.1 0.4-0.1 0.6-0.1c0.3 0 0.6 0.1 1 0.3     c0.4 0.1 0.6 0.3 0.8 0.5l1.8-1.9c-0.5-0.4-1-0.7-1.7-0.9C41 50.2 40.4 50.2 39.8 50.2 M38 43.2c-0.3 0-0.7 0-1.1 0     c-0.4 0-0.8 0-1.2 0c-0.4 0.1-0.8 0.2-1.1 0.3c-0.4 0.1-0.7 0.3-1 0.5c-0.3 0.2-0.5 0.5-0.7 0.8c-0.1 0.3-0.2 0.7-0.2 1.2     c0 0.4 0.1 0.8 0.2 1c0.1 0.3 0.3 0.6 0.6 0.8c0.2 0.2 0.5 0.4 0.9 0.5c0.3 0.1 0.7 0.2 1.1 0.2c0.5 0 0.9-0.1 1.4-0.3     c0.4-0.2 0.8-0.6 1.1-1v1h2.3v-4c0-0.6-0.1-1.2-0.2-1.7s-0.3-0.9-0.6-1.3c-0.3-0.3-0.6-0.6-1-0.8c-0.5-0.2-1.1-0.3-1.8-0.3     c-0.7 0-1.3 0.1-1.9 0.3c-0.6 0.2-1.1 0.5-1.6 1l1.3 1.3c0.3-0.3 0.6-0.5 0.9-0.7c0.4-0.2 0.8-0.3 1.2-0.3c0.4 0 0.7 0.1 1 0.4     C37.8 42.4 38 42.8 38 43.2 M35 45.9c0-0.3 0.1-0.5 0.3-0.6c0.2-0.2 0.4-0.3 0.7-0.3c0.2-0.1 0.5-0.2 0.8-0.2c0.3 0 0.5 0 0.7 0     H38v0.5c0 0.3 0 0.5-0.1 0.7c-0.1 0.2-0.3 0.3-0.4 0.5c-0.2 0.1-0.4 0.2-0.6 0.3c-0.2 0.1-0.4 0.1-0.7 0.1s-0.5-0.1-0.8-0.2     C35.1 46.4 35 46.2 35 45.9 M27.1 36.8h-4.3v11.5h4.8c0.5 0 1-0.1 1.5-0.2c0.5-0.1 1-0.3 1.4-0.5c0.4-0.3 0.8-0.6 1.1-1     c0.2-0.4 0.3-0.9 0.3-1.5c0-0.4 0-0.7-0.1-1c-0.1-0.3-0.3-0.6-0.5-0.8c-0.2-0.3-0.5-0.5-0.8-0.6c-0.3-0.2-0.7-0.3-1-0.3v0     c0.6-0.2 1-0.5 1.4-0.9c0.4-0.4 0.5-1 0.5-1.6c0-0.6-0.1-1.1-0.3-1.5c-0.3-0.4-0.6-0.7-1-0.9c-0.4-0.2-0.9-0.3-1.5-0.4     C28.1 36.8 27.6 36.8 27.1 36.8 M28.2 39.2c0.2 0.1 0.3 0.2 0.4 0.3c0.1 0.2 0.2 0.4 0.2 0.6c0 0.3-0.1 0.5-0.2 0.6     c-0.1 0.2-0.2 0.3-0.4 0.3c-0.2 0.1-0.3 0.2-0.5 0.3c-0.2 0.1-0.4 0.1-0.6 0.1h-1.9v-2.5H27c0.2 0 0.4 0 0.6 0.1     C27.9 38.9 28 39 28.2 39.2 M25.3 46v-2.6h2c0.2 0 0.4 0 0.6 0c0.3 0 0.5 0.1 0.6 0.2c0.2 0.1 0.4 0.2 0.6 0.3     c0.2 0.2 0.3 0.4 0.3 0.7c0 0.2-0.1 0.4-0.2 0.6c-0.1 0.2-0.2 0.4-0.4 0.5S28.4 46 28.2 46c-0.2 0-0.4 0-0.6 0H25.3 M54.8 40.3     L52 43.5V36h-2.5v12.3H52v-4h0l2.7 4h3l-3.2-4.3l3.2-3.6H54.8 M46.5 42.5c0.2 0.1 0.4 0.2 0.5 0.4l1.6-1.6     c-0.4-0.4-0.8-0.7-1.3-0.9c-0.6-0.1-1.1-0.2-1.6-0.2c-0.6 0-1.1 0.1-1.6 0.3c-0.6 0.2-1.1 0.4-1.5 0.8s-0.7 0.8-0.9 1.3     c-0.3 0.5-0.4 1.1-0.4 1.7c0 0.6 0.1 1.2 0.4 1.7c0.2 0.5 0.5 0.9 0.9 1.3c0.4 0.4 0.9 0.6 1.5 0.8c0.5 0.2 1.1 0.4 1.6 0.4     c0.5 0 1-0.1 1.6-0.3c0.5-0.2 0.9-0.5 1.3-0.9l-1.6-1.6c-0.1 0.2-0.3 0.3-0.5 0.4c-0.2 0.1-0.4 0.2-0.7 0.2     c-0.6 0-1.1-0.2-1.4-0.5c-0.4-0.4-0.5-0.9-0.5-1.5c0-0.6 0.2-1.1 0.5-1.5c0.3-0.4 0.8-0.5 1.4-0.5C46.1 42.3 46.4 42.3 46.5 42.5     z""/>      </Canvas>    </Canvas>    <Canvas Name=""g67"">      <Path xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" Name=""Layer1_0_2_STROKES"" StrokeThickness=""5"" Stroke=""#FF000000"" StrokeLineJoin=""Round"" StrokeStartLineCap=""Round"" StrokeEndLineCap=""Round"" Data=""    M87.9 22.1c3.3 0 5 1.7 5 5c-0.7 15.6-0.7 31.2 0 46.7c0 3.3-1.7 5-5 5H13.1c-3.3 0-5-1.7-5-5c0.7-15.5 0.7-31.1 0-46.7    c0-3.3 1.7-5 5-5H87.9z""/>    </Canvas>  </Canvas></Canvas>";
+            }
+        }
 
     }
 
