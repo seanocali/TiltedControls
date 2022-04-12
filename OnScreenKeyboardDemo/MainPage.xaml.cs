68b7ddfd01b031a1fa9ec113f1c7b82116be0b34
Sean O'Neil <seanoneilcalifornia@gmail.com>
2022-04-11 22:41:08 -0700

    Namespace change

--- OnScreenKeyboardDemo/MainPage.xaml.cs
+++ OnScreenKeyboardDemo/MainPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using TiltedControls.Shared;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
--- TiltedControls.Shared/OnScreenKeyboard.xaml.cs
+++ TiltedControls.Shared/OnScreenKeyboard.xaml.cs
@@ -16,7 +16,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace TiltedControls.Shared
+namespace TiltedControls
 {
     public sealed partial class OnScreenKeyboard : UserControl
     {
